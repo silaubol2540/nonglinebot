1fa8d7a240fd4ed2a313768a0b2228dd615c45e9
unknown <REGRMUTR11@gmailcom>
2019-09-24 10:29:22 +0700

    update

--- index.js
+++ index.js
@@ -126,7 +126,7 @@ function handleMessageEvent(event) {
             'type': 'text',
             'text' : 'https://grade.rmutr.ac.th/แบบคำร้องงานทะเบียน/'
         }
-    }else if (eventText === 'สวัสดีครับ') {
+    }else if (eventText === 'ติดต่อสอบถาม') {
         msg = {
             'type': 'text',
             'text' : 'สำนักส่งเสริมวิชาการและงานทะเบียนมหาวิทยาลัยเทคโนโลยีราชมงคลรัตนโกสินทร์ยินดีให้บริการครับ'
